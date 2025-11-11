17831de8189edb5d8a1897ba2f6794a69fa97be5
Farhad <farhadmikky@gmail.com>
2025-11-11 12:42:49 +0600

    Implemented JWT-based authentication system (login, registration, logout, and middleware protection)

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,10 +2,72 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    // Register 
+
+    public function register(Request $req)
+    {
+        $data = $req->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|confirmed|min:4',
+        ]);
+
+        $user = User::create([
+             'name' => $data['name'],
+             'email' => $data['email'],
+             'password' => Hash::make($data['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+    //  Login
+    public function login(Request $req){
+        $credentials = $req->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+        
+        if(!$token = auth()->attempt($credentials)){
+            return response()->json(['error' => 'Invalid Credentials'], 401);
+        }
+
+        return response()->json([
+            'user' => auth()->user(),
+            'token' => $token,
+        ]);
+    }
+
+  // current user
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        $newToken = auth()->refresh();
+
+        return response()->json([
+            'token' => $newToken,
+        ]);
+    }
 }
